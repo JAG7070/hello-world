f8b5f018d77f548c87c53a91a6c3eb156f7c3a78
JAG7070 <36303422+JAG7070@users.noreply.github.com>
2018-02-17 22:26:48 +0100

    Added two py-files

---
+++ my_turtle.py
@@ -0,0 +1,50 @@
+from graphics import *
+import math
+
+
+class MyTurtle:
+    _x = 0
+    _y = 0
+    _angle = 0
+
+    def __init__(self, win, x, y, angle):
+        self._win = win
+        self._x = x
+        self._y = y
+        self._angle = angle
+
+    def forward(self, length):
+        # Calculate new x and y by _angle and length
+        dx = math.sin(self.get_radians()) * length
+        dy = math.cos(self.get_radians()) * length
+        p2 = Point(self._x + dx, self._y + dy)
+        line = Line(Point(self._x, self._y), p2)
+        g = 0 #g = 255 - int(length * 3)
+        if g < 0:
+            g = 0
+        line.setFill(color_rgb(0, g, 0))
+        line.draw(self._win)
+        self._x = p2.x
+        self._y = p2.y
+
+    def get_radians(self):
+        return math.pi * self._angle / 180.0
+
+    def pos(self):
+        pos_tuple = (self._x, self._y)
+        return pos_tuple
+
+    def heading(self):
+        return self._angle
+
+    def right(self, d_angle):
+        self._angle = self._angle + d_angle
+
+    def setheading(self, heading):
+        self._angle = heading
+
+    def left(self, d_angle):
+        self._angle = self._angle - d_angle
+
+    def goto(self, pos):
+        self._x, self._y = pos
---
+++ simple_tree.py
@@ -0,0 +1,87 @@
+from graphics import *
+from my_turtle import MyTurtle
+from random import uniform
+
+WIN_WIDTH = 700
+WIN_HEIGHT = 600
+
+RAND_ANGLE_MAX = 30
+RAND_FACTOR_MAX = 0.2
+
+wn = GraphWin("Simple tree", WIN_WIDTH, WIN_HEIGHT, autoflush=False)
+tree = MyTurtle(wn, 50, 50, 0)
+angle = 5
+dividend = 1.4
+
+
+def clear(win):
+    for item in win.items[:]:
+        item.undraw()
+    # win.update()
+
+
+def branch(length, use_randomness=True):
+    if length < 2:
+        return
+
+    tree.forward(length)
+
+    pos = tree.pos()
+    heading = tree.heading()
+
+    # randomness
+    rand_angle_r = 0.0
+    rand_factor_r = 1.0
+    rand_angle_l = 0.0
+    rand_factor_l = 1.0
+    if use_randomness:
+        rand_angle_r = uniform(-RAND_ANGLE_MAX, RAND_ANGLE_MAX)
+        rand_factor_r = uniform(1-RAND_FACTOR_MAX, 1+RAND_FACTOR_MAX)
+        rand_angle_l = uniform(-RAND_ANGLE_MAX, RAND_ANGLE_MAX)
+        rand_factor_l = uniform(1-RAND_FACTOR_MAX, 1+RAND_FACTOR_MAX)
+
+    # branch to the right
+    tree.right(angle + rand_angle_r)
+    branch(rand_factor_r * length / dividend, use_randomness)
+
+    # branch to the left
+    tree.goto(pos)
+    tree.setheading(heading)
+    tree.left(angle + rand_angle_l)
+    branch(rand_factor_l * length / dividend, use_randomness)
+
+
+def main():
+    global angle
+    use_randomness = True
+    redraw = True
+    while True:
+        tree.goto((WIN_WIDTH/2, WIN_HEIGHT - 30))
+        tree.setheading(180)
+
+        if redraw:
+            clear(wn)
+            branch(100, use_randomness)
+            wn.update()
+
+        #click_point = wn.checkMouse()
+        #click_point = wn.getMouse()
+        #if click_point != None and click_point.x < 20 and click_point.y < 20:
+        #    break
+
+        #redraw = False
+
+        key = wn.checkKey()
+        redraw = key != None and key != '' #or key != ''
+
+        if key == 'q':
+            angle = angle + 1
+        elif key == 'a':
+            angle = angle - 1
+        elif key == 'c':
+            break
+
+    wn.close()
+
+
+main()
